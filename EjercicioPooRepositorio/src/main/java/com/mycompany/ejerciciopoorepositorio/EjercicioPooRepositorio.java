72b5b0ae67fbb806288a159a83ffa1e393778791
andresMV8 <162385032+andresMV8@users.noreply.github.com>
2024-03-05 19:13:23 -0500

    CreacionProyecto 05/04/24

---
+++ EjercicioPooRepositorio/src/main/java/com/mycompany/ejerciciopoorepositorio/EjercicioPooRepositorio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejerciciopoorepositorio;
+
+/**
+ *
+ * @author Unicomfacauca
+ */
+public class EjercicioPooRepositorio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
